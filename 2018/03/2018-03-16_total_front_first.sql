d9918748f5e43405512728137d08a55d461cfa34
xgp123 <1099223026@qq.com>
2018-03-16 17:24:37 +0800

    03-16

--- 2018/03/2018-03-16_total_front_first.sql
+++ 2018/03/2018-03-16_total_front_first.sql
@@ -1,3 +1,55 @@
+/*Table structure for table `gr_user_verify` */
+
+DROP TABLE IF EXISTS `gr_user_verify`;
+
+CREATE TABLE `gr_user_verify` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT COMMENT '推送ID',
+  `user_id` int(11) unsigned NOT NULL DEFAULT '0' COMMENT '用户ID',
+  `type` varchar(50) unsigned NOT NULL DEFAULT '' COMMENT '验证类型：1 手机验证, 2 邮箱验证, 3 google验证',
+  `create_time` int(11) unsigned NOT NULL DEFAULT '0' COMMENT '添加时间',
+  `update_time` int(11) unsigned NOT NULL DEFAULT '0' COMMENT '修改时间',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=31 DEFAULT CHARSET=utf8 COMMENT='用户验证推送';
+
+
+-- Setting表新增字段group(time 2018-03-01 10:06)
+	+ ALTER TABLE `gr_app_v2`.`gr_setting` ADD COLUMN `group` TINYINT(2) UNSIGNED DEFAULT 0 NOT NULL COMMENT '分组' AFTER `value`;
+
+-- Setting更新group字段数据（time 2018-03-01 10:13）
+	+ UPDATE `gr_app_v2`.`gr_setting` SET `group` = '1' WHERE `id` = '1';
+	+ UPDATE `gr_app_v2`.`gr_setting` SET `group` = '1' WHERE `id` = '2';
+	+ UPDATE `gr_app_v2`.`gr_setting` SET `group` = '1' WHERE `id` = '3';
+	+ UPDATE `gr_app_v2`.`gr_setting` SET `group` = '1' WHERE `id` = '4';
+	+ UPDATE `gr_app_v2`.`gr_setting` SET `group` = '1' WHERE `id` = '5';
+	+ UPDATE `gr_app_v2`.`gr_setting` SET `group` = '2' WHERE `id` = '6';
+	+ UPDATE `gr_app_v2`.`gr_setting` SET `group` = '3' WHERE `id` = '7';
+	+ UPDATE `gr_app_v2`.`gr_setting` SET `group` = '3' WHERE `id` = '8';
+	+ UPDATE `gr_app_v2`.`gr_setting` SET `group` = '4' WHERE `id` = '9';
+	+ UPDATE `gr_app_v2`.`gr_setting` SET `group` = '4' WHERE `id` = '10';
+	+ UPDATE `gr_app_v2`.`gr_setting` SET `group` = '4' WHERE `id` = '11';
+	+ UPDATE `gr_app_v2`.`gr_setting` SET `group` = '5' WHERE `id` = '12';
+	+ UPDATE `gr_app_v2`.`gr_setting` SET `group` = '6' WHERE `id` = '13';	
+
+
+-- Setting表新增字段type(time 2018-03-01 10:21)
+	+ ALTER TABLE `gr_app_v2`.`gr_setting` ADD COLUMN `type` TINYINT(2) UNSIGNED DEFAULT 0 NOT NULL COMMENT '类型（0 input输入框，1 时间控件，2 select多选框）' AFTER `value`; 
+
+-- Setting更新type字段数据（time 2018-03-01 10:50）
+	+ UPDATE `gr_app_v2`.`gr_setting` SET `type` = '0' WHERE `id` = '1';
+	+ UPDATE `gr_app_v2`.`gr_setting` SET `type` = '0' WHERE `id` = '2';
+	+ UPDATE `gr_app_v2`.`gr_setting` SET `type` = '0' WHERE `id` = '3';
+	+ UPDATE `gr_app_v2`.`gr_setting` SET `type` = '0' WHERE `id` = '4';
+	+ UPDATE `gr_app_v2`.`gr_setting` SET `type` = '0' WHERE `id` = '5';
+	+ UPDATE `gr_app_v2`.`gr_setting` SET `type` = '0' WHERE `id` = '6';
+	+ UPDATE `gr_app_v2`.`gr_setting` SET `type` = '1' WHERE `id` = '7';
+	+ UPDATE `gr_app_v2`.`gr_setting` SET `type` = '1' WHERE `id` = '8';
+	+ UPDATE `gr_app_v2`.`gr_setting` SET `type` = '2' WHERE `id` = '9';
+	+ UPDATE `gr_app_v2`.`gr_setting` SET `type` = '0' WHERE `id` = '10';
+	+ UPDATE `gr_app_v2`.`gr_setting` SET `type` = '0' WHERE `id` = '11';
+	+ UPDATE `gr_app_v2`.`gr_setting` SET `type` = '2' WHERE `id` = '12';
+	+ UPDATE `gr_app_v2`.`gr_setting` SET `type` = '0' WHERE `id` = '13';
+
+
 -- 短信、邮箱模板修新增记录
 
 -- email-template数据插入
